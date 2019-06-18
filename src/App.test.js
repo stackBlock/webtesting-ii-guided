5ce6e266391232224c9a0dd41f177b9e6bb30131
anthony <stackchain@gmail.com>
2019-06-18 15:02:56 -0400

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/React';
 import App from './App';
+import { fireEvent } from '@testing-library/react/dist';
+import '@testing-library/react/cleanup-after-each';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+
+  it('renders without errors', () => {
+    const queries = render(<App />);
+    console.log('\n*** queries', queries);
+
+    queries.getByText(/hello world/i);
+  });
+
+  it('renders without failing', () => {
+    render(<App />);
+  });
+
+
+
+
 });
+describe('Greet Button', () => {
+  it('says hello developers', () => {
+    const { getByText, queryByText } = render(<App />);
+
+    const button = getByText(/greet/i);
+
+    fireEvent.click(button);
+
+    expect(queryByText(/hello developers/i)).toBeTruthy();
+  });
+});
+
